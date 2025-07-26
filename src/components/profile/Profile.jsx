16b600fe32a8e8daa1eae21493ba9bc4aee75aa7
jugendrakashyap <kjugendra63@gmail.com>
2025-07-27 00:04:31 +0530

    update my portfolio

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -13,7 +13,7 @@ function Profile() {
             <div className='details_box'>
                 <div className='detail_box'>
                     <div className='icon'>
-                        <img id='email_img' src="src/assets/email.png" alt="" />
+                        <img className="icon_img" src="https://cdn-icons-png.flaticon.com/512/4338/4338894.png" alt="" />
                     </div>
                     <div>
                         <p className='label'>EMAIL</p>
@@ -21,30 +21,42 @@ function Profile() {
                     </div>
                 </div>
                 <div className='detail_box'>
-                    <div className='icon'></div>
+                    <div className='icon'>
+                        <img className="icon_img" src="https://cdn-icons-png.flaticon.com/512/159/159832.png " alt="" />
+                    </div>
                     <div>
                         <p className='label'>MOBILE & WHATSAPP</p>
                         <p>+91 7217205976</p>
                     </div>
                 </div>
                 <div className='detail_box'>
-                    <div className='icon'></div>
+                    <div className='icon'>
+                        <img className='icon_img' src="https://cdn-icons-png.flaticon.com/512/747/747310.png" alt='' />
+                    </div>
                     <div>
                         <p className='label'>BIRTHDAY</p>
                         <p>15/07/2006</p>
                     </div>
                 </div>
                 <div className='detail_box'>
-                    <div className='icon'></div>
+                    <div className='icon'>
+                        <img className='icon_img' src="https://cdn-icons-png.flaticon.com/512/2838/2838912.png" alt='' />
+                    </div>
                     <div>
                         <p className='label'>LOCATION</p>
                         <p>Ahmedabad, Gujrat</p>
                     </div>
                 </div>
                 <div className='social_box'>
-                    <img className='social_links' src="#" alt="" />
-                    <img className='social_links' src="#" alt="" />
-                    <img className='social_links' src="#" alt="" />
+                    <a href="https://www.linkedin.com/in/jugendrakashyap">
+                        <img className='social_links' src="https://cdn-icons-png.flaticon.com/512/61/61109.png" alt="" />
+                    </a>
+                    <a href="https://github.com/jugendrakashyap">
+                        <img className='social_links' src="https://cdn-icons-png.flaticon.com/512/25/25657.png" alt="" />
+                    </a>
+                    <a href="#">
+                        <img className='social_links' src="https://cdn-icons-png.flaticon.com/512/1006/1006771.png" alt="" />
+                    </a>
                 </div>
             </div>
         </section>
